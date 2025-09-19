f2798671a73d6959b8cd5f63869849df3fa2afc8
Alice-Lemerre <lemerre.alice@gmail.com>
2025-09-19 09:47:25 +0200

    aout et fix chatbox

--- src/Filter/Home.js
+++ src/Filter/Home.js
@@ -7,6 +7,7 @@ import books from './BookData';
 import RadioSelector from "./RadioSelector";
 import SelectBar from "./SelectBar";
 import AutocompleteSearch from "./AutocompleteSearch";
+import ChatBox from '../chatbox/ChatBox';
 
 
 
@@ -59,6 +60,7 @@ const Home = ({minPrice, maxPrice, onMinPriceChange, onMaxPriceChange }) => {
             availability={availability}
             author={author}
           />
+          <ChatBox />
       </div>
   );
 };
